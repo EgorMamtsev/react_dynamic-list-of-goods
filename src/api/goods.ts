b2be276d8fe948464ff2f3944124124f5403fe90
Egor Mamtsev <egormamcev@MacBook-Air.local>
2025-08-22 14:07:28 -0500

    catch added

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -4,16 +4,43 @@ import { Good } from '../types/Good';
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
 export function getAll(): Promise<Good[]> {
-  return fetch(API_URL).then(response => response.json());
+  return fetch(API_URL)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error();
+      }
+
+      return response.json() as Promise<Good[]>;
+    })
+    .catch(error => {
+      // eslint-disable-next-line no-console
+      console.log('Fetch failed:', error);
+
+      return [];
+    });
 }
 
 export const get5First = () => {
   // return getAll().then(goods => goods); // sort and get the first 5
-  return getAll().then(goods =>
-    goods.sort((a, b) => a.name.localeCompare(b.name)).slice(0, 5),
-  );
+  return getAll()
+    .then(goods =>
+      goods.sort((a, b) => a.name.localeCompare(b.name)).slice(0, 5),
+    )
+    .catch(error => {
+      // eslint-disable-next-line no-console
+      console.log('Fetch failed:', error);
+
+      return [];
+    });
 };
 
 export const getRedGoods = () => {
-  return getAll().then(goods => goods.filter(item => item.color === 'red')); // get only red
+  return getAll()
+    .then(goods => goods.filter(item => item.color === 'red'))
+    .catch(error => {
+      // eslint-disable-next-line no-console
+      console.log('Fetch failed:', error);
+
+      return [];
+    }); // get only red
 };
